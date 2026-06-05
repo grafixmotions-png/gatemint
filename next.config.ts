8dfb8497d44d639b5d703c10cc0f6e30f37648ae
grafixmotions-png <grafixmotions@gmail.com>
2026-06-05 23:06:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   turbopack: {
+    // Lock the watch root specifically to the project folder to prevent scanning the home directory
     root: path.resolve(typeof __dirname !== 'undefined' ? __dirname : process.cwd()),
   },
 };
 
 export default nextConfig;
-
